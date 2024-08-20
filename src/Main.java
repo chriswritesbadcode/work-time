e91bf7796cecfd4958c94121a2d1bf82a629a6a6
Chris <chris.anderson@teletalkhub.com>
2024-08-20 13:35:38 +0300

    add jframe basics

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main{
-    public static void main(String args[]){
-        System.out.println("Hello world");
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class Main {
+
+    public static void main(String args[]) {
+        JFrame window = new JFrame("Work Time");
+        JPanel panel = new JPanel();
+        JLabel text = new JLabel("Work Time");
+        JButton button = new JButton();
+
+        window.setSize(300, 300);
+        window.setLocationRelativeTo(null);
+
+        panel.add(button);
+        panel.add(text);
+
+        window.add(panel);
+
+        window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        window.setVisible(true);
     }
 }
